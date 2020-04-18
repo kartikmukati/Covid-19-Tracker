3c85a81959a74e908513e68167b58955f8953353
kartik mukati <kartikmukati2021@gmail.com>
2020-04-18 15:41:06 +0530

    3

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin')
 const error = require('./controller/error')
 const path = require('path')
+
+const port = process.env.PORT || 3000
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,4 +15,4 @@ app.use(express.static(path.join(__dirname,'public')))
 app.use(adminRoutes)
 app.use(error.get404)
 
-app.listen(3000)
+app.listen(port)
